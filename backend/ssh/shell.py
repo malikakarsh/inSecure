23b510a55592caae39a9151c14cec05c0f0bcd1f
Akarsh Malik <malikakarsh@gmail.com>
2021-03-28 02:51:03 +0530

    [backend] fix default shell for user

--- backend/ssh/shell.py
+++ backend/ssh/shell.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
 import os
-import subprocess,shlex
+import subprocess
+import shlex
+
+# path = f"/home/zest/activity.log"
+# file = open(path, 'w+')
+SSH_USER = "USERNAME"
 
 
-def main():
-    username = "sshUser"
-    while True:
+def shell_cmd(cmd):
+    output = shlex.split(cmd)
+    if "cd" in output:
         try:
-            x = input(f"\u001b[32m{username}@enSecure\u001b[0m -> ")
-
-            if x == 'exit':
-                break
-
-            print("Hello")
-            os.system(x)
+            os.chdir(output[1])
+        except Exception as e:
+            print("")
+    else:
+        try:
+            # ip = os.popen("echo $SSH_CLIENT").read()
+            # file.write(f"{ip} {cmd}")
+            os.system(cmd)
 
         except Exception as e:
             print(e)
+
+
+def main():
+    while True:
+        try:
+            x = input(f"\u001b[32m{SSH_USER}@inSecure\u001b[0m -> ")
+
+            if x == 'exit':
+                # file.close()
+                break
+
+            shell_cmd(x)
+
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
     main()
