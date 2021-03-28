30fb6a784b393f35ca17ede66ba250bd894b26e2
anurag goyal <anuraggoyal.awr@gmail.com>
2021-03-28 00:03:49 +0000

    [Feat.] ssh bruteforce protection

--- backend/ssh/startup.sh
+++ backend/ssh/startup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-service ssh start
+dummy_ssh_login() {
+    sshpass -p 'abcd' ssh -o StrictHostKeyChecking=no username@localhost 1>/dev/null 2>&1
+    return 0
+}
 
+sudo service ssh start
+
+sudo service rsyslog start
+
+# just so that rsyslog created /var/log/auth.log
+dummy_ssh_login
+
+sudo service fail2ban start
 
 /bin/bash
